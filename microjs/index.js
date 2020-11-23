1b2c7a3526873347fadfdefc0ea6a1b2d63b247c
Janos Dobronszki <dobronszki@gmail.com>
2020-11-23 14:27:19 +0100

    Fixes

--- microjs/index.js
+++ microjs/index.js
@@ -28,9 +28,29 @@ function transformToAssocArray(prmstr) {
   return params;
 }
 
-function getCookieValue(a) {
-  var b = document.cookie.match("(^|;)\\s*" + a + "\\s*=\\s*([^;]+)");
-  return b ? b.pop() : "";
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+
+function eraseCookie(name) {
+  document.cookie = name + "=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
 }
 
 function get(path, namespace, params, callback) {
@@ -109,8 +129,15 @@ function initModal() {
           namespace: "backend",
         },
       },
-      function (text) {
-        console.log(text);
+      function (rsp) {
+        if (!rsp && !rsp.token) {
+          return;
+        }
+        setCookie("micro_access", rsp.token.access_token, 30);
+        setCookie("micro_refresh", rsp.token.refresh_token, 30);
+        setCookie("micro_expiry", rsp.token.expiry, 30);
+        // hide modal display if things are all good
+        modal.style.display = "none";
       }
     );
   };
