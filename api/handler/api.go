38d4141e40430feaec47e5cf69ce6e1047d42caf
Janos Dobronszki <dobronszki@gmail.com>
2021-03-19 16:32:02 +0000

    f

--- api/handler/api.go
+++ api/handler/api.go
@@ -129,9 +129,9 @@ func (e *V1) Serve(ctx context.Context, req *pb.Request, rsp *pb.Response) error
 		var template = Handlebars.compile(source);
 		var rendered = template(view);
 		
-		var el = document.createElement('html');
-		el.innerHTML = rendered
-		morphdom(document.getElementById('` + id.String() + `'), el);
+		//var el = document.createElement('html');
+		//el.innerHTML = rendered
+		morphdom(document.getElementById('` + id.String() + `'), rendered);
 	}
 	var Embed = {
 		render: _render,
