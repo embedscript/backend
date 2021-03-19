c313fbbcbfb03769d9cb626500f8f3e5d2ecc201
Janos Dobronszki <dobronszki@gmail.com>
2021-03-19 16:46:29 +0000

    f

--- api/handler/api.go
+++ api/handler/api.go
@@ -131,7 +131,7 @@ func (e *V1) Serve(ctx context.Context, req *pb.Request, rsp *pb.Response) error
 		
 		//var el = document.createElement('html');
 		//el.innerHTML = rendered
-		morphdom(document.getElementById('` + id.String() + `'), '<div id="` + id.String() + `">' + rendered + '</div>');
+		morphdom(document.getElementById('` + id.String() + `'), rendered, {childrenOnly: true});
 	}
 	var Embed = {
 		render: _render,
