defca493b17ff4ab3eb79b7d581964df30cac493
Janos Dobronszki <dobronszki@gmail.com>
2021-03-19 16:48:56 +0000

    f

--- api/handler/api.go
+++ api/handler/api.go
@@ -131,7 +131,7 @@ func (e *V1) Serve(ctx context.Context, req *pb.Request, rsp *pb.Response) error
 		
 		//var el = document.createElement('html');
 		//el.innerHTML = rendered
-		morphdom(document.getElementById('` + id.String() + `'), rendered, {childrenOnly: true});
+		morphdom(document.getElementById('` + id.String() + `'), rendered);
 	}
 	var Embed = {
 		render: _render,
